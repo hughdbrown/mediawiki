207caa65b137de04c2296cae0dd62c575bf207e5
Hugh Brown <hbrown@amplify.com>
2015-04-22 09:39:59 -0600

    Remove extraneous files

--- mediawiki/build_wiki.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-# docker build -t mediawiki-data mediawiki-data/
-docker build -t mediawiki .
--- mediawiki/run-wiki.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker run \
-	-v /data/wiki:/data/wiki \
-	-p 80:80 \
-	-d \
-	mediawiki
-
