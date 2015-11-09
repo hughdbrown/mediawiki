3f79b6bb58fdb210abaa8d249258fea6e8bc89f0
Hugh Brown <hughdbrown@yahoo.com>
2015-11-09 10:30:11 -0700

    Add check for running docker service

--- run-wiki.sh
+++ run-wiki.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+service_name=docker
+if ((ps -ef | grep -v grep | grep $service_name | wc -l) > 0)
+then
+    echo $service_name is running
+else
+    service $service_name start
+fi
+
 docker run \
 	-v /data/wiki:/data/wiki \
 	-p 80:80 \
